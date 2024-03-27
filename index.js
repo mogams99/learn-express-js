c4bc0d56f6d8c15daf6090da05d980ae1d745437
Mochammad Gamal <mogams1811@gmail.com>
2024-03-27 22:06:31 +0700

    chore: :construction: introduce route express js

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    console.log(`We got request`);
-    res.send({ message: `Hello Express`})
+// app.use((req, res) => {
+//     console.log(`We got request`);
+//     res.send({ message: `Hello Express`})
+// });
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'This is home page',
+    });
+});
+
+app.get('/cats', (req, res) => {
+    res.send({
+        message: 'This is cats page',
+    });
+});
+
+app.post('/cats', (req, res) => {
+    res.send({
+        message: 'This is cats page with post method',
+    });
+});
+
+app.get('*', (req, res) => {
+    res.send({
+        message: 'Page not found',
+    });
 });
 
 app.listen(8080, () => {
